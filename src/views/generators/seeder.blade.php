dd0f774081592dc6e309e85c03824bd5f053d4a5
Sid Millspaugh <sid@evilblue.com>
2015-04-02 08:15:40 +0200

    Fixed seeder

--- src/views/generators/seeder.blade.php
+++ src/views/generators/seeder.blade.php
@@ -9,6 +9,7 @@ class SubdivisionsSeeder extends Seeder {
      */
     public function run()
     {
+        $country_id = array();
         //Empty the subdivisions table
         DB::table(\Config::get('laravel-subdivisions::table_name'))->delete();
 
@@ -19,14 +20,14 @@ class SubdivisionsSeeder extends Seeder {
             // Get the country ID for this subdivision
             if(\Config::get('laravel-subdivisions::country_table_name') && \Config::get('laravel-subdivisions::iso_3166_2_row_name'))
             {
-                $country_id = DB::select('select id from '.\Config::get('laravel-subdivisions::country_table_name').' where '.'.\Config::get('laravel-subdivisions::iso_3166_2_row_name').'.' = ? LIMIT 1', array($country['iso_3166_2']));
+                $country_id = DB::select('select id from '.\Config::get('laravel-subdivisions::country_table_name').' where '.\Config::get('laravel-subdivisions::iso_3166_2_row_name').' = \''.$country['country'].'\' LIMIT 1');
             }
 
             DB::table(\Config::get('laravel-subdivisions::table_name'))->insert(array(
                 'id' => $subdivisionId,
                 'country' => ((isset($country['country'])) ? $country['country'] : null),
                 'country_name' => ((isset($country['country_name'])) ? $country['country_name'] : null),
-                'country_id' => ((isset($country_id)) ? $country_id : null),
+                'country_id' => ((isset($country_id[0])) ? $country_id[0]->id : null),
                 'iso_3166_2' => $country['iso_3166_2'],
                 'region' => ((isset($country['region'])) ? $country['region'] : null),
                 'region_alt' => ((isset($country['region_alt'])) ? $country['region_alt'] : null)
